ee8b67088b0e1ae984d2dccfdcc3ee2232a34833
Shubham Kumar <Sk@gmail.com>
2025-02-25 11:29:31 +0530

    Day 1 of 120

--- DAY1/query1.sql
+++ DAY1/query1.sql
@@ -1,10 +1,14 @@
-create DATABASE Challenge;
+create DATABASE Challenge; 
+-- Creating a database
 
 USE Challenge;
+-- initializing the database
+
 CREATE TABLE movies(
 Id int,
 Title varchar(225),
 Year int);
+-- Creating a table for movies
 
 INSERT INTO movies(Id,Title,Year) values
 (1,'Kung fu Panda',1997),
@@ -12,3 +16,4 @@ INSERT INTO movies(Id,Title,Year) values
 (3,'Star Wars',1977),
 (4,'Jurassic Park',1979),
 (5,'Iron man',1989);
+-- inserting values into the table
